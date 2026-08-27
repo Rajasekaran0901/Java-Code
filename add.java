9cfe12e9212a393034332a6c9d37b4b08fe860d5
RAJASEKARAN S <rajasekaranselvam55@gmail.com>
2026-08-27 21:23:29 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class add{
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("a:");
+        System.out.println("b:");
+        System.out.println("c:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a+b+c);
+    }
+}
