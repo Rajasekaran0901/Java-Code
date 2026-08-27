fbf009a93d64880cf122ca558216fe9f69d43c20
RAJASEKARAN S <rajasekaranselvam55@gmail.com>
2026-08-27 21:36:36 +0530

    Create atm.java

---
+++ atm.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class arraylist {
+   arraylist() {
+   }
+
+   public static void main(String[] var0) {
+      ArrayList var1 = new ArrayList();
+      var1.add(10);
+      var1.add(20);
+      var1.add(30);
+      System.out.println(var1);
+   }
+}
